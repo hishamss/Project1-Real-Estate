ffaa18185c9d34c30f6ebd4fa3c0010aac8cc576
Jennifer Kruk <jenkruk@gmail.com>
2020-03-24 10:27:41 -0700

    properties section complete

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,7 +3,9 @@ var result = [];
 var Lat, Lon;
 $(document).ready(function() {
   // API call when search button clicked
-  $("#search").on("click", function() {
+  $("#search").on("click", function(event) {
+     //Prevents the page from refreshing when the button is clicked
+    event.preventDefault();
     zipcode = $(".userInput")
       .val()
       .trim();
@@ -37,12 +39,13 @@ $(document).ready(function() {
           radius +
           "postal_code=" +
           zipcode +
-          "&offset=0&limit=100",
+          "&offset=0&limit=12",
         method: "GET",
         headers: {
           "x-rapidapi-host": "realtor.p.rapidapi.com",
           "x-rapidapi-key": "7f6807f23bmsh0797a1d4ca8a067p10f0bajsnc10b5c239d52"
         }
+
       }).done(function(data) {
         result = data.listings;
         console.log(result);
@@ -54,124 +57,29 @@ $(document).ready(function() {
           } else {
             Photo = "assets/images/logo.jpg";
           }
-          if (i % 4 == 0 && i !== 0) {
-            $(".properties").append('<div class="w-100"></div>');
             $(".properties").append(
-              '<div class="col"><div class="card PropertyCard" data-id="' +
+              '<div class="col"><div class="card " data-id="' +
                 i +
                 '"><img src="' +
                 Photo +
-                '" class="card-img-top" height="200"><p class="overlay">' +
+                '" class="card-img-top"><p class="price">' +
                 result[i].price +
-                '</p><div class="card-body"><div class="row"><div class="col">' +
+                '</p><div class="card-body">' +
                 result[i].beds +
                 " beds | " +
                 result[i].baths +
                 " baths | " +
                 result[i].sqft.split(" ")[0] +
-                ' sqft</div><div class="w-100"></div><div class="col"><strong>' +
+                ' sqft</div><div class="col">' +
                 result[i].address +
-                "</strong></div>" +
-                "</div></div></div></div>"
+                "</div></div></div>"
             );
-          } else {
-            $(".properties").append(
-              '<div class="col"><div class="card PropertyCard" data-id="' +
-                i +
-                '"><img src="' +
-                Photo +
-                '" class="card-img-top" height="200"><p class="overlay">' +
-                result[i].price +
-                '</p><div class="card-body"><div class="row"><div class="col">' +
-                result[i].beds +
-                " beds | " +
-                result[i].baths +
-                " baths | " +
-                result[i].sqft.split(" ")[0] +
-                ' sqft</div><div class="w-100"></div><div class="col"><strong>' +
-                result[i].address +
-                "</strong></div></div></div>"
-            );
-          }
         }
       });
     }
   });
 
-  // $(function() {
-  //   // IMPORTANT: Fill in your client key
-  //   var clientKey =
-  //     "js-0CPYQ0Fzjgzo483l0DqjZocTE7K18SexqKR4W118LrbeK8SC97yALL8ATccwidKz";
-
-  //   var cache = {};
-  //   var container = $("#example1");
-  //   var errorDiv = container.find("div.text-error");
-
-  //   /** Handle successful response */
-  //   function handleResp(data) {
-  //     // Check for error
-  //     if (data.error_msg) errorDiv.text(data.error_msg);
-  //     else if ("city" in data) {
-  //       // Set city and state
-  //       container.find("input[name='city']").val(data.city);
-  //       container.find("input[name='state']").val(data.state);
-  //     }
-  //   }
-
-  //   // Set up event handlers
-  //   container
-  //     .find("input[name='zipcode']")
-  //     .on("keyup change", function() {
-  //       // Get zip code
-  //       var zipcode = $(this)
-  //         .val()
-  //         .substring(0, 5);
-  //       if (zipcode.length == 5 && /^[0-9]+$/.test(zipcode)) {
-  //         // Clear error
-  //         errorDiv.empty();
-
-  //         // Check cache
-  //         if (zipcode in cache) {
-  //           handleResp(cache[zipcode]);
-  //         } else {
-  //           // Build url
-  //           var url =
-  //             "https://www.zipcodeapi.com/rest/" +
-  //             clientKey +
-  //             "/info.json/" +
-  //             zipcode +
-  //             "/radians";
-
-  //           // Make AJAX request
-  //           $.ajax({
-  //             url: url,
-  //             dataType: "json"
-  //           })
-  //             .done(function(data) {
-  //               handleResp(data);
-
-  //               // Store in cache
-  //               cache[zipcode] = data;
-  //             })
-  //             .fail(function(data) {
-  //               if (
-  //                 data.responseText &&
-  //                 (json = $.parseJSON(data.responseText))
-  //               ) {
-  //                 // Store in cache
-  //                 cache[zipcode] = json;
-
-  //                 // Check for error
-  //                 if (json.error_msg) errorDiv.text(json.error_msg);
-  //               } else errorDiv.text("Request failed.");
-  //             });
-  //         }
-  //       }
-  //     })
-  //     .trigger("change");
-  // });
-
-  $(document).on("click", ".PropertyCard", function() {
+  $(document).on("click", ".card", function() {
     console.log("called");
     var Id = $(this).attr("data-id");
     Lat = result[Id].lat;
